1ae8a3dfffe0a194e5b815e8671e73637c27484e
omarupk33 <kkomar12334@gmail.com>
2026-02-04 22:53:44 +0300

    still working on it

--- bst.js
+++ bst.js
@@ -7,21 +7,69 @@ class treeNode{
 }
 
 
-function bst(list, start= 0 , end = list.length - 1){
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node === null || node === undefined) {
+    return;
+    }
+
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+}
+
+function Tree(array){
+
+    let root = null
+
+    array.sort()
+
+    function buildTree(array, start= 0 , end = array.length - 1){
     if (start > end){
-     return list
+     return array
     }
 
     let mid = Math.ceil((start + end)/2)
-
-    let root = new treeNode(list[mid])
-    root.data = list[mid]
-    root.left = bst(list,start, mid - 1)
-    root.right = bst(list, mid + 1, end)
+    let root = new treeNode(array[mid])
+    root.data = array[mid]
+    root.left = buildTree(array,start, mid - 1)
+    root.right = buildTree(array, mid + 1, end)
 
 
     return root
 }
 
+    root = buildTree(array)
 
-console.log(bst([1,2,3,4,5,6,7]))
+
+    function includes(value){
+        for (let element of array){
+            if(element === value){
+                return true
+            }
+        }
+        return false
+    }
+
+    function insertion(value){
+        for (let element of array){
+        if(value > element){
+            array.push(value)
+            array.sort()
+            break
+        }
+        }
+
+        root = buildTree(array)
+        prettyPrint(root)
+    }
+
+
+    return {root,  includes, insertion}
+}
+
+let thatOneTree = new Tree([1,2,4,3,5,6,7,9])
+
+thatOneTree.insertion(0)
+
+// prettyPrint(thatOneTree.root)
+// console.log(thatOneTree.includes(6))
